b314d1b058f46e1f6057bfb34130005ef6668662
tapsa <tapsa872@gmail.com>
2018-01-09 16:22:48 +0200

    added hobby list 'add hobbies' and 'delete hobbies'

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -1,19 +1,37 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'user',
-  template: `<h1>Hello {{name}}</h1>
-  <p><strong>Email: </strong>{{email}}</p>
-  <p><strong>Address: </strong>{{address.street}} {{address.city}}, {{address.state}}</p>
-  <button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies" : "Show Hobbies"}}</button>
-  <div *ngIf="showHobbies">
-  <h3>Hobbies</h3>
-    <ul>
-        <li *ngFor="let hobby of hobbies">
-            {{hobby}}
-        </li>
-    </ul>
-  </div>
+    selector: 'user',
+    template: `<h1>{{name}}</h1>
+    <p><strong>Email: </strong>{{email}}</p>
+    <p><strong>Address: </strong>{{address.street}} {{address.city}}, {{address.state}}</p>
+    <button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies" : "Show Hobbies"}}</button>
+    <div *ngIf="showHobbies">
+    <h3>Hobbies</h3>
+        <ul>
+            <li *ngFor="let hobby of hobbies; let i = index">
+                {{hobby}}<button (click)="deleteHobby(i)">x</button>
+            </li>
+        </ul>
+    </div>
+    <hr />
+    <h3>Edit User</h3>
+    <form (submit)="addHobby(hobby.value)">
+        <label>Add Hobby: </label> <br/>
+        <input type="text" #hobby /> <br />
+    </form>
+    <form >
+        <label>Name: </label><br/>
+        <input type="text" name="name" [(ngModel)]="name" />
+        <label>Email: </label><br/>
+        <input type="text" name="email" [(ngModel)]="email" />
+        <label>Street: </label><br/>
+        <input type="text" name="address.street" [(ngModel)]="address.street" />
+        <label>City: </label><br/>
+        <input type="text" name="address.city" [(ngModel)]="address.city" />
+        <label>State: </label><br/>
+        <input type="text" name="address.state" [(ngModel)]="address.state" />
+    </form>
   `,
 })
 export class UserComponent  { 
@@ -42,7 +60,14 @@ constructor() {
       } else {
         this.showHobbies = true;
     }
-      
+  }
+
+  addHobby(hobby) {
+    this.hobbies.push(hobby);
+  }
+  
+  deleteHobby(i) {
+      this.hobbies.splice(i, 1);
   }
 }
 
